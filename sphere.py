b2656882aaf5119908322063ee5f8c68d0aff6a6
Sally Mbula <87859986+SallyMbula@users.noreply.github.com>
2024-01-30 12:52:53 +0300

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,10 @@
+import math
+
+# Read radius of sphere
+r = float(input("Enter radius of sphere: "))
+
+# Calculate volume
+volume = (4 / 3) * math.pi * r**3
+
+# Print volume
+print("Volume of sphere: ", volume)
